2ac898200f4503a27523448b94ae4759ad8a3c23
nbrown-dsl <nbrown@dwightlondon.org>
2021-01-14 13:56:11 +0000

    form post method

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 
 app = Flask(__name__)
 app.debug = True
@@ -13,3 +13,16 @@ def about():
     
     names = ["Mr Brown","Turing","Gates"]
     return render_template('about.html',names=names)
+
+@app.route('/subscribe')
+def subscribe():
+    
+    return render_template('subscribe.html')
+
+@app.route('/form', methods=["POST"])
+def form():
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    email = request.form.get("email")
+    
+    return render_template('form.html', first_name = first_name, last_name = last_name, email = email)
