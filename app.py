896473910669d8d5519ce3af7d2b45593b8da3b5
nbrown-dsl <nbrown@dwightlondon.org>
2021-01-14 14:06:32 +0000

    form posting and data passed

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template, request
 
 app = Flask(__name__)
-app.debug = True
+subscribers = []
 
 @app.route('/')
 def index():
@@ -24,5 +24,6 @@ def form():
     first_name = request.form.get("first_name")
     last_name = request.form.get("last_name")
     email = request.form.get("email")
+    subscribers.append(f"{first_name} {last_name} | {email}")
     
-    return render_template('form.html', first_name = first_name, last_name = last_name, email = email)
+    return render_template('form.html', subscribers = subscribers)
