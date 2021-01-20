4ec3ebfdd0450b76445187fbe9071856de2a1e08
nbrown-dsl <nbrown@dwightlondon.org>
2021-01-20 15:37:39 +0000

    alerts added

--- app.py
+++ app.py
@@ -21,9 +21,19 @@ def subscribe():
 
 @app.route('/form', methods=["POST"])
 def form():
+    
     first_name = request.form.get("first_name")
     last_name = request.form.get("last_name")
     email = request.form.get("email")
+
+    if not first_name or not last_name or not email:
+        error_statement = "all fields required"
+        return render_template("subscribe.html",
+        error_statement = error_statement,
+        first_name = first_name,
+        last_name = last_name,
+        email = email)
+        
     subscribers.append(f"{first_name} {last_name} | {email}")
     
     return render_template('form.html', subscribers = subscribers)
