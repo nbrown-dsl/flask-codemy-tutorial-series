dfce8231046b775deae3156ef5e403ae140a87ff
nbrown-dsl <nbrown@dwightlondon.org>
2021-01-22 09:00:19 +0000

    images

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template, request
+# import smtplib
 
 app = Flask(__name__)
 subscribers = []
@@ -25,6 +26,12 @@ def form():
     first_name = request.form.get("first_name")
     last_name = request.form.get("last_name")
     email = request.form.get("email")
+    #from tutorial #6 - skip this as challenges with password security and authentication
+    # message = "You have subscribed to my email newsletter"
+    # server = smtplib.smtp("smtp.gmail.com", 587)
+    # server.starttls()
+    # server.login("nbrown@dwightlondon.org",os.getenv("GMPASSWORD"))
+    # server.sendmail("nbrown@dwightlondon.org",email,message)
 
     if not first_name or not last_name or not email:
         error_statement = "all fields required"
